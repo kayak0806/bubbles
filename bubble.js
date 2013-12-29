01249f27ae395120faaccc2bc2129246e1b163f1
Kaitlin <kayak0806@yahoo.com>
2013-12-28 20:37:45 -0500

    removed unpausing bug
    individuals no longer un-pause when they're not supposed to

--- bubble.js
+++ bubble.js
@@ -1,19 +1,23 @@
 //*** Initializing ***
-// Extract bubbles
+// Extract bubbles (div id="ocean", ul, li)
 var elm = document.getElementById("ocean");
 var kids = elm.getElementsByTagName("ul");
 var bubbles = kids[0].getElementsByTagName("li")
 
-
-
-// Put Bubble State objects in 'dictionary'
+// Create Bubble State objects and put in 'dictionary'
 var bubbleStates = new Object
 for (var i = bubbles.length - 1; i >= 0; i--) {
 	var title = bubbles[i].getElementsByTagName("a")[0].getAttribute('title')
-
 	var state = new State(title)
 	bubbleStates[title] = state	
 }
+
+// Extract button(s)
+var pauseButton = document.getElementById("pauseButton")
+var fireButton = document.getElementById("fireButton")
+
+var pauseCheck = document.getElementById("pauseCheck")
+var pauseBox = pauseCheck.getElementsByTagName("input")[0]
 
 
 //*** Objects ***
@@ -57,6 +61,12 @@ function placeRand(state){
 	state.y = Math.random()*(window.innerHeight-(100+200))+100
 }
 
+// Give a bubble state a specific location
+function place(state,x,y){
+	state.x = x
+	state.y = y
+}
+
 // Give a state a new direction with probability prob
 function dirRand(state, prob){
 	var randx = Math.random()-prob
@@ -85,11 +95,12 @@ function updateState(state){
 	// if near right edge
 	if (state.x > window.innerWidth - margin-200) 	state.dirx = -1
 
-	if (!state.paused){
-		state.x += 1*state.dirx
-		state.y += 1*state.diry
+	if (!state.paused && !pauseBox.checked){
+		state.x += 5*state.dirx
+		state.y += 5*state.diry
 	}
 }
+
 
 // pause and play bubble
 function pause(state){
@@ -98,10 +109,13 @@ function pause(state){
 function play(state){
 	state.paused = false
 }
+function alternate(state){
+	state.paused = !state.paused
+}
 
-//*** Doing stuff *** 
+//*** Activation *** 
 
-//set pause/play mouseover action
+//set pause/play mouseover action for all bubbles
 for (var i = bubbles.length - 1; i >= 0; i--) {	
 	bubbles[i].onmouseover=function() {
 		var title = this.getElementsByTagName("a")[0].getAttribute('title')
@@ -112,7 +126,8 @@ for (var i = bubbles.length - 1; i >= 0; i--) {
 		play(getState(title)); 
 	}
 }
-// Animate state
+
+// Animate bubble states
 function animate(state){
 	setInterval(function() {
 		updateState(state)
@@ -123,7 +138,7 @@ for (var key in bubbleStates){
 	state = getState(key)
 	updateState(state)
 	updateCSS(state)
-	animate(getState(key))	
+	animate(getState(key))	//comment this to pause and troubleshoot
 }
 
 // *** Playing ***
